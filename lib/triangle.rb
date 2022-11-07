fb453d9dc5a0b5a70cbb9bed36516af3aba6b27d
Brook Nega <brooknega21@gmail.com>
2022-11-07 12:56:01 -0500

    commit tree 11dcef2132df75ac61aeb2aa398536363df80240

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+  attr_reader :side1, :second_side, :third_side
+  def initialize(side1, second_side, third_side)
+    @side1 = side1
+    @second_side = second_side
+    @third_side = third_side
+  end
+
+  def kind 
+    validate_triangle
+    if side1 == third_side && second_side == side1
+      :equilateral
+    elsif side1 == third_side || third_side == second_side or side1 == second_side
+      :isosceles
+    else  :scalene
+    end
+  end
+
+  def positive_sides?
+    [side1, third_side, second_side].all?(&:positive?)
+  end
+
+  def triangle_inequality?
+    side1 + third_side > second_side && side1 + second_side > third_side && third_side + second_side > side1
+  end
+
+  def validate_triangle
+    raise TriangleError unless positive_sides? && triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
 end
+
+first_triangle = Triangle.new(2,1,2)
+
+p first_triangle.validate_triangle
